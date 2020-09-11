58e787ad3a862ce1d94f1f9d7c6dd43865a2a723
Stefan Ziegler <edi.gonzales@gmail.com>
2020-09-11 09:11:17 +0200

    wip

--- src/main/java/HelloFX.java
+++ src/main/java/HelloFX.java
@@ -1,4 +1,3 @@
-  
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -9,13 +8,18 @@ public class HelloFX extends Application {
 
     @Override
     public void start(Stage stage) {
-System.setProperty("prism.lcdtext", "false");
-System.setProperty("prism.text", "t2k");
- 
-       String javaVersion = System.getProperty("java.version");
+        System.setProperty("prism.lcdtext", "false");
+        System.setProperty("prism.text", "t2k");
+
+        String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
         Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        //l.setStyle("-fx-font: 14 'IBM Plex Sans';");
+        
+        
         Scene scene = new Scene(new StackPane(l), 640, 480);
+        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
         stage.setScene(scene);
         stage.show();
     }
